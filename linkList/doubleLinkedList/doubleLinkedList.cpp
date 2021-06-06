01a4bfd138d13a2603ef1fef36e55baec60c8e1f
Lip <931011668@qq.com>
2021-06-06 17:42:00 +0800

    double

--- linkList/doubleLinkedList/doubleLinkedList.cpp
+++ linkList/doubleLinkedList/doubleLinkedList.cpp
@@ -3,16 +3,19 @@ using namespace std;
 
 
 typedef struct DoubleLinkedList {
-	singleLinkedList* next,*pre;
+	DoubleLinkedList* next,*pre;
 	int data;
 }node, * pnode;
 
-pnode linkHeadInsert();
+pnode initList();
 void display(pnode);
-node* initLinkedList();
-void change(pnode, int, int);
-bool deleteNode(pnode, int);
-bool insertNode(pnode, int, int);
+
+int main() {
+	pnode temp = NULL;
+	temp = initList();
+	display(temp);
+}
+
 
 pnode initList() {
 	pnode head = (pnode)malloc(sizeof(node));
@@ -20,13 +23,46 @@ pnode initList() {
 		cout << "Error generating head node" << endl;
 		exit(-1);
 	}
+	pnode temp = head;
 
 
+	for (int i = 0; i < 5; i++) {
+		pnode body = (pnode)malloc(sizeof(node));
+		if (NULL == body) {
+			cout << "Error generating body node" << endl;
+			exit(-1);
+		}
+		body->pre = NULL;
+		body->next = NULL;
+		body->data = i;
+
+		temp->next = body;
+		body->pre = temp;
+		temp = body;
+	}
+	return head;
 }
 
-
-
-int main()
-{
-    std::cout << "Hello World!\n";
+void display(pnode head) {
+	if (head == NULL) {
+		cout << "Error displaying list" << endl;
+		exit(-1);
+	}
+	pnode list = head;
+	while (list->next != NULL) {
+		list = list->next;
+		cout << list->data<<endl;
+	}
 }
+
+/*
+增删改查与单链表并无多少差异，需要注意的是：
+	以插入为例子：
+	新建结点Newbody;(默认temp的位置为插入结点的前一个位置)
+	Newbody->next = temp->next;
+	temp->next->pre = Newbody;
+	Newbody->pre = temp;
+	temp->next = Newbody;//最后才修改，防止操作失误导致temp丢失后面的结点。
+*/
+
+
