87383291637f6a79dacc84819224b581c6ba927c
Lip <931011668@qq.com>
2021-06-06 17:25:18 +0800

    DoubleLinked List

---
+++ linkList/doubleLinkedList/doubleLinkedList.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+
+typedef struct DoubleLinkedList {
+	singleLinkedList* next,*pre;
+	int data;
+}node, * pnode;
+
+pnode linkHeadInsert();
+void display(pnode);
+node* initLinkedList();
+void change(pnode, int, int);
+bool deleteNode(pnode, int);
+bool insertNode(pnode, int, int);
+
+pnode initList() {
+	pnode head = (pnode)malloc(sizeof(node));
+	if (head == NULL) {
+		cout << "Error generating head node" << endl;
+		exit(-1);
+	}
+
+
+}
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
